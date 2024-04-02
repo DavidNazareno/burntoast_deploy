3f24a38fb81bc41f5ba0e7713a950145aee350f0
Jesus David Nazareno Cardona <DnNzareno@gmail.com>
2024-04-02 22:45:32 +0200

    Add overflow-x: hidden to img in toast-8a2c9b.webflow.css

---
+++ _next/static/chunks/app/page-5676147a42a77349.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{8721:function(e,t,n){Promise.resolve().then(n.t.bind(n,7275,23)),Promise.resolve().then(n.t.bind(n,9254,23))},7275:function(e){e.exports={title:"start-screen_title__suY7P",logo:"start-screen_logo__Qu2ae"}}},function(e){e.O(0,[254,146,672,744],function(){return e(e.s=8721)}),_N_E=e.O()}]);
